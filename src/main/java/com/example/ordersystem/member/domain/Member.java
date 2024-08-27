701bbd0e76a13a85b5f20c68ff6ef88d4f98e985
leem5514 <ikaros0569@gmail.com>
2024-08-27 17:19:53 +0900

    fix

--- src/main/java/com/example/ordersystem/member/domain/Member.java
+++ src/main/java/com/example/ordersystem/member/domain/Member.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
 
     private String name;
